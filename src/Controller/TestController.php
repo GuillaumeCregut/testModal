235c6476eb4acf83aa53a8d2109ce17fc9693a7f
Guillaume Crégut <gcregut@free.fr>
2024-11-14 11:30:46 +0100

    Create CRUD

---
+++ src/Controller/TestController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Test;
+use App\Form\TestType;
+use App\Repository\TestRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\UX\Turbo\TurboStreamResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\UX\Turbo\Helper\TurboStream;
+
+final class TestController extends AbstractController
+{
+    #[Route('/{id}', name: 'app_test_index', methods: ['GET'])]
+    public function index(TestRepository $testRepository, int $id = 0): Response
+    {
+        if ($id > 0) {
+            $test = $testRepository->find($id);
+            if ($test) {
+                // return new TurboStreamResponse(
+                //     $this->render('', ['test' => $test]),
+                //     'replace',
+                //     'test_' . $test->getId()
+                // );
+            }
+        }
+        return $this->render('test/index.html.twig', [
+            'tests' => $testRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_test_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $test = new Test();
+        $form = $this->createForm(TestType::class, $test);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($test);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_test_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('test/new.html.twig', [
+            'test' => $test,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_test_show', methods: ['GET'])]
+    public function show(Test $test): Response
+    {
+        return $this->render('test/show.html.twig', [
+            'test' => $test,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_test_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Test $test, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TestType::class, $test);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_test_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('test/edit.html.twig', [
+            'test' => $test,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_test_delete', methods: ['POST'])]
+    public function delete(Request $request, Test $test, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $test->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($test);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_test_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/modal/{id}', name: 'app_test_modal')]
+    public function modal(Test $test): Response
+    {
+        return $this->render('test/edit_modal.html.twig', [
+            // 'form' => $form->createView(),
+            'product' => $test,
+        ]);
+    }
+}
