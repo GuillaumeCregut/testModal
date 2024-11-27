66e5d59f02f3c1d65f730cffae953fed35870ba9
Guillaume Crégut <gcregut@free.fr>
2024-11-27 15:43:14 +0100

    ca march pas ici

--- src/Twig/Components/Quotations/QuotationLineComp.php
+++ src/Twig/Components/Quotations/QuotationLineComp.php
@@ -5,6 +5,7 @@ namespace App\Twig\Components\Quotations;
 use App\Entity\Quotation;
 use Symfony\Component\Form\FormInterface;
 use App\Form\Quotations\QuotationFormType;
+use App\Form\Quotations\QuotationLineFormType;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\LiveCollectionTrait;
@@ -12,31 +13,17 @@ use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[AsLiveComponent]
-final class QuotationFormComp extends AbstractController
+final class QuotationLineComp extends AbstractController
 {
     use DefaultActionTrait;
     use LiveCollectionTrait;
 
-    #[LiveProp(fieldName: 'formData')]
-    public ?Quotation $quotation;
+    #[LiveProp()]
+    public QuotationLineFormType $formLine;
 
-    #[LiveProp(writable: true)]
-    public float $totalValueHT=0;
+    #[LiveProp()]
+    public ?Quotation $quotation; 
 
-    #[LiveProp(writable: true)]
-    public float $totalValueTTC=0;
-
-    public function calculateTotalValueTTC(float $totalValueHT): float
-    {
-        $totalValueTTC = $totalValueHT * 1.2;
-        return $totalValueTTC;
-    }
-
-    public function calculateTotalValueHT(float $totalValueTTC): float
-    {
-        $totalValueHT = $totalValueTTC / 1.2;
-        return $totalValueHT;
-    }
     protected function instantiateForm(): FormInterface
     {
         return $this->createForm(
